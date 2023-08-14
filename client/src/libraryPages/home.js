58a1c4d519671c7cb24d0438054cf30ebc4e07dd
Naamafb <naamafb12@gmail.com>
2023-08-14 16:30:39 +0300

    a

--- client/src/libraryPages/home.js
+++ client/src/libraryPages/home.js
@@ -2,6 +2,6 @@ import { useNavigate } from "react-router-dom";
 import { useParams, useEffect, useState } from "react";
 import { FaTrash, FaEdit, FaSave } from 'react-icons/fa';
 function home(){
- return <p>welcome to libarary</p>
+ return <p>welcome to library</p>
 }
 export default home;
